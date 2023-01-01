f5f98d96e2f5c89dd9905c17fd0de42a09e197e0
Jonathan <jonathanmoorevla@gmail.com>
2022-12-31 18:45:17 -0600

    Reorg app folder. Continue with models/seeds

---
+++ climb-control-server/app/setter.rb
@@ -0,0 +1,4 @@
+class Setter < ActiveRecord::Base
+
+
+end
--- climb-control-server/db/seeds.rb
+++ climb-control-server/db/seeds.rb
@@ -1,7 +1,7 @@
 puts "🌱 Seeding messages..."
 
 Climber.create(name: "Chris", age: 29, gym_member: TRUE)
-Climber.create(name: "Samantha", age: 52, gym_member: TRUE)
+Climber.create(name: "Anastasia", age: 52, gym_member: TRUE)
 Climber.create(name: "Jesse", age: 22, gym_member: FALSE)
 Climber.create(name: "Joshua", age: 33, gym_member: TRUE)
 Climber.create(name: "Alexa", age: 29, gym_member: FALSE)
@@ -9,9 +9,10 @@ Climber.create(name: "Miguel", age: 32, gym_member: TRUE)
 Climber.create(name: "Britt", age: 31, gym_member: TRUE)
 Climber.create(name: "Bailey", age: 29, gym_member: FALSE)
 
-
-
-
+Setter.create(name: "Seth", guest_setter: FALSE)
+Setter.create(name: "Samantha", guest_setter: FALSE)
+Setter.create(name: "Sinbad", guest_setter: FALSE)
+Setter.create(name: "Stephanie", guest_setter: TRUE)
 
 
 
