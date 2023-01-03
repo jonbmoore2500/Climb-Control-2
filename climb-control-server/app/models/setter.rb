1556e929f971858531029dac8c2741460daaf07c
Jonathan <jonathanmoorevla@gmail.com>
2023-01-02 20:00:13 -0600

    Build easiest/hardest problem methods for Setter

--- climb-control-server/app/models/setter.rb
+++ climb-control-server/app/models/setter.rb
@@ -1,4 +1,13 @@
 class Setter < ActiveRecord::Base
     has_many :problems 
 
+
+    def hardest_problem
+        self.problems.order(difficulty: :desc).first
+    end
+
+    def easiest_problem
+        self.problems.order(difficulty: :asc).first
+    end
+
 end
