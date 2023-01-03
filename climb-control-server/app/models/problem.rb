caf3233cd3a7f6665f001738bf1b0ecc6e169dee
Jonathan <jonathanmoorevla@gmail.com>
2023-01-02 19:58:00 -0600

    Build days_remaining Problem method

--- climb-control-server/app/models/problem.rb
+++ climb-control-server/app/models/problem.rb
@@ -1,5 +1,13 @@
+require 'date'
+
 class Problem < ActiveRecord::Base
+
     has_many :climbs 
     has_many :climbers, through: :climbs
     belongs_to :setter 
+
+    def days_remaining
+        (self.date_to_remove - Date.today).to_i
+    end
+
 end
