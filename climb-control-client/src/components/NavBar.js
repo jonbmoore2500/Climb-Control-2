cde85e513c6dc7bcddfec0423ab753b47ae29554
Jonathan <jonathanmoorevla@gmail.com>
2023-01-07 15:39:18 -0600

    Adjust react-router-dom version, change keywords

---
+++ climb-control-client/src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react"
+//import { NavLink } from "react-router-dom";
+
+function NavBar() {
+
+    return (
+        <div className="navBar">
+            Hello, this will be my navbar
+        </div>
+    )
+}
+
+export default NavBar
