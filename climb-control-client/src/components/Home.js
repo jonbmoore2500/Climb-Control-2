2a5e5bb6dec9930ffe83817ef6aa4380fb6af622
Jonathan <jonathanmoorevla@gmail.com>
2023-01-16 12:25:22 -0600

    Incorporate number_climbers into ProblemCards

--- climb-control-client/src/components/Home.js
+++ climb-control-client/src/components/Home.js
@@ -3,8 +3,19 @@ import React from 'react'
 function Home() {
 
     return(
-        <div>
-            <p>This is my phase 3 project. It organizes climbers, setters, routes, and climbs.</p>
+        <div id="homeDiv">
+            <p>This is my phase 3 project. It organizes Climbers, Setters, Routes, and Climbs.</p>
+            <p>
+                In the Climbers tab you can view all the Climbers and 
+                assign them Climbs to pair them with Problems they have successfully climbed.
+                This might update some information about the climber, such as the hardest Problem they have climbed
+                or the average difficulty of all the Problems they have climbed.
+            </p>
+            <p>
+                In the Problems tab you can view all the available Problems, along with information about them.
+                Only Problems that are still available to be climbed are displayed. You can also use the form to 
+                create new Problems or edit or delete an existing Problem.
+            </p>
         </div>
     )
 }
--- climb-control-client/src/components/ProblemCard.js
+++ climb-control-client/src/components/ProblemCard.js
@@ -6,11 +6,20 @@ function ProblemCard({problemObj, handleClick, selected}) {
         handleClick(problemObj.id)
     }
 
+    const numberClimberText = 
+        problemObj.number_climbers == 1 ? 
+        `${problemObj.number_climbers} climber has climbed this problem.` :
+        `${problemObj.number_climbers} climbers have climbed this problem.`
+
     return (
         <div className="cards">
             <div onClick={onClick} className={`problem-${selected ? "active" : ""}`} >
                 V{problemObj.difficulty} set by {problemObj.setter.name}
                 . It was set on {problemObj.date_set} and has {problemObj.days_remaining} days left!
+                <br></br>
+                {problemObj.number_climbers == 8 ? 
+                "All of the climbers have climbed this problem!!" : 
+                numberClimberText}
             </div>
         </div>
     )
